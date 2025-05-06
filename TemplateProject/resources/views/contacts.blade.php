2ef2ebe0f61242fc9d20fd45d1339dbbbc6235fb
Liya Rizvanova <liyarizvanova@gmail.com>
2025-05-06 20:50:25 +0300

    Template Project added

---
+++ TemplateProject/resources/views/contacts.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+
+@section('title', 'Contacts')
+
+@section('content')
+    <div class="col-12 mt-4">
+        <h1>Contact Us</h1>
+        <p>If you have any questions, feel free to reach out to us.</p>
+
+        <ul class="list-unstyled">
+            <li>Email: support@example.com</li>
+            <li>Phone: +1 234 567 890</li>
+            <li>Address: 123 Main Street, Laravel City</li>
+        </ul>
+    </div>
+@endsection
